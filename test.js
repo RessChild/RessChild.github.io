a579129f3451faf95401fd804ab6f6d5626d35bc
RessChild <39401832+RessChild@users.noreply.github.com>
2020-07-09 16:37:28 +0900

    transition 개념 + vw / vh
    화면 클릭 시 스크롤 업
    화면 크기에 맞추어 크기조절

--- test.js
+++ test.js
@@ -1,16 +1,34 @@
-const p2 = document.querySelector(".js-2"),
-    p3 = document.querySelector(".js-3");
+const title = document.querySelector(".js-title"),
+    content = document.querySelector(".js-content");
 
-var f_timer;
+const SCROLL_UP = "scroll-up",
+    SCROLL_UP_W = "scroll-up-wrapper",
+    NAME = "PROGRAMMER 최현수";
+
+
+// class list 에 중복되는 요소가 있으면 가장 뒤에 등자한 요소로 씀
+// 그러니까 CSS 에서 이벤트 처리때 추가되는 놈들은 전부 하단으로 몰아넣을 것
+
+function imgSize(tf){ // 이미지 크기조절
+    const big = "30vh", small = "10vh";
+    title.querySelector("img").style.maxWidth = tf ? big : small;
+    title.querySelector("img").style.maxHeight = tf ? big : small;
+}
 
 // flex 는 set interver 로도 안되는듯..
 function flexControl(){
-    let val = p2.style.flex;
-    console.log(val);
-    if (val >= 2) clearInterval(f_timer);
-    else {
-        p2.style.flex = val + 0.2;
-        console.log(p2.style.flex);
+    console.log("크기 변경");
+    if (!title.classList.contains(SCROLL_UP)){ // 해당 클래스명을 갖고있지 않으면
+        title.classList.add(SCROLL_UP); // 추가하고 화면 올림
+        content.style.flex = 7;
+        imgSize(false); // 이미지 크기 조절
+        title.querySelector(".title-wrapper").classList.add(SCROLL_UP_W)
+    }
+    else{ // 있으면
+        title.classList.remove(SCROLL_UP); // 제거하고 화면 내림
+        content.style.flex = 0;
+        imgSize(true);
+        title.querySelector(".title-wrapper").classList.remove(SCROLL_UP_W)
     }
 }
 
@@ -20,9 +38,16 @@ function wheelEvent(e){
 
 function clickEvent(e){
     console.log("클릭!!");
-    setInterval(flexControl,1000);
+    flexControl();
 }
 
-// scroll 은 페이지가 이동할때만 반응하는듯..
-document.addEventListener("mousewheel", wheelEvent);
-document.addEventListener("click", clickEvent);
+
+function init(){
+    // scroll 은 페이지가 이동할때만 반응하는듯..
+    document.addEventListener("mousewheel", wheelEvent);
+    document.querySelector(".js-title").addEventListener("click", clickEvent);
+
+    title.querySelector("#js-name").innerHTML = NAME;
+}
+
+init();
