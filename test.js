f8a0db15f5d93b594a2b8cfa39a794382f85920b
RessChild <39401832+RessChild@users.noreply.github.com>
2020-07-10 15:14:04 +0900

    display: none
    사이드바 온오프 기능

--- test.js
+++ test.js
@@ -1,13 +1,22 @@
+/* HTMLDOM 객체 */
 const title = document.querySelector(".js-title"),
     content = document.querySelector(".js-content"),
-    bar = document.querySelectorAll(".bar-img");
+    bar = document.querySelector("#content-bar"),
+    bar_li = bar.querySelectorAll("li"),
+    side = document.querySelector(".content-side"),
+    side_btn = document.querySelector("#side-btn");
 
+/* 여러 고정 상수 (문자열) */
 const SCROLL_UP = "scroll-up",
     SCROLL_UP_W = "scroll-up-wrapper",
     NAME = "PROGRAMMER 최현수",
-    SELECT = "",
-    HOVER = "",
-    DEFAULT = "";
+    SELECT = "./resource/list_select.png",
+    DEFAULT = "./resource/list_default.png",
+    BTN_CLICK = "hide-side",
+    BTN_ON = "./resource/ex.png";
+
+/* 기타 페이지용 변수 */
+let li_idx = -1; // 현재 선택된 페이지 번호 (초기값 = -1)
 
 // class list 에 중복되는 요소가 있으면 가장 뒤에 등자한 요소로 씀
 // 그러니까 CSS 에서 이벤트 처리때 추가되는 놈들은 전부 하단으로 몰아넣을 것
@@ -44,6 +53,28 @@ function clickEvent(e){
     flexControl();
 }
 
+function listClick(e) {
+//    console.log([].indexOf.call(nodes, e.target));
+    console.log(e.target, e.currentTarget);
+
+    // 그냥 target 은 실제 클릭된 대상
+    // currentTarget 은 해당 이벤트를 발생시킨 최상위 개체를 의미하는듯..
+    bar_li.forEach(element => {
+        element.querySelector(".bar-img").src = (e.currentTarget.key == element.key ? SELECT : DEFAULT);
+    });
+    li_idx = e.currentTarget.key; // 선택된 페이지로 결정
+}
+
+function btnClick(e){
+    console.log("사이드바 버튼 클릭", e);
+
+    if(!side.classList.contains(BTN_CLICK)){
+        side.classList.add(BTN_CLICK);
+    }
+    else{
+        side.classList.remove(BTN_CLICK);
+    }
+}
 
 function init(){
     // scroll 은 페이지가 이동할때만 반응하는듯..
@@ -52,13 +83,19 @@ function init(){
 
     title.querySelector("#js-name").innerHTML = NAME;
     // 위쪽 바는 document.title 로 변경 ( 윈도우 아님 )
-    document.title = "현수's 포트폴리오";
+    document.title = "포트폴리오";
 
     console.log(bar)
+
     // 이부분 차후에 이미지 관련 처리해줘야함..
-    for(let k = 0; k<bar.length; k++){
-        bar[k].src = "./resource/ex.png" 
+    for(let k = 0; k<bar_li.length; k++){
+        bar_li[k].key = k; /* 각 리스트에게 키값 부여 */
+        bar_li[k].querySelector(".bar-img").src = DEFAULT;
+        bar_li[k].addEventListener('click', listClick);
     }
+
+    side_btn.src = BTN_ON;
+    side_btn.addEventListener('click', btnClick);
 }
 
 init();
